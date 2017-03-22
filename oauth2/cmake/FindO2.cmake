644b3932586618b184a8d4fe6dcbe065bf34a549
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-03-22 03:02:17 -0600

    Add FindPackageHandleStandardArgs support to FindO2 CMake module

--- oauth2/cmake/FindO2.cmake
+++ oauth2/cmake/FindO2.cmake
@@ -14,32 +14,38 @@
 #    O2_LIBRARY
 #    O2_LIBRARY_STATIC
 
-FIND_PATH(O2_INCLUDE_DIR o2.h
-  PATHS
-  /usr/include
-  /usr/local/include
-  "$ENV{LIB_DIR}/include"
-  $ENV{INCLUDE}
-  PATH_SUFFIXES o2
-  )
-FIND_LIBRARY(O2_LIBRARY NAMES o2
-  PATHS
-  /usr/local/lib
-  /usr/lib
-  "$ENV{LIB_DIR}/lib"
-  "$ENV{LIB}"
-  )
-FIND_LIBRARY(O2_LIBRARY_STATIC NAMES libo2.a libo2_static.a o2_static
-  PATHS
-  /usr/local/lib
-  /usr/lib
-  "$ENV{LIB_DIR}/lib"
-  "$ENV{LIB}"
-  )
-
-IF (O2_INCLUDE_DIR AND (O2_LIBRARY OR O2_LIBRARY_STATIC) )
+IF (O2_INCLUDE_DIR AND (O2_LIBRARY OR O2_LIBRARY_STATIC))
    SET(O2_FOUND TRUE)
-ENDIF (O2_INCLUDE_DIR AND (O2_LIBRARY OR O2_LIBRARY_STATIC) )
+
+ELSE (O2_INCLUDE_DIR AND (O2_LIBRARY OR O2_LIBRARY_STATIC))
+
+  FIND_PATH(O2_INCLUDE_DIR o2.h
+    PATHS
+    /usr/include
+    /usr/local/include
+    "$ENV{LIB_DIR}/include"
+    $ENV{INCLUDE}
+    PATH_SUFFIXES o2
+    )
+  FIND_LIBRARY(O2_LIBRARY NAMES o2
+    PATHS
+    /usr/local/lib
+    /usr/lib
+    "$ENV{LIB_DIR}/lib"
+    "$ENV{LIB}"
+    )
+  FIND_LIBRARY(O2_LIBRARY_STATIC NAMES libo2.a libo2_static.a o2_static
+    PATHS
+    /usr/local/lib
+    /usr/lib
+    "$ENV{LIB_DIR}/lib"
+    "$ENV{LIB}"
+    )
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(O2 DEFAULT_MSG O2_LIBRARY O2_INCLUDE_DIR)
+
+ENDIF (O2_INCLUDE_DIR AND (O2_LIBRARY OR O2_LIBRARY_STATIC))
 
 IF (O2_FOUND)
    IF (NOT O2_FIND_QUIETLY)
